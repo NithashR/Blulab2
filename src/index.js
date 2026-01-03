2c6d87357d476417a152dbe8611666146b2245d2
Nithash Rajendram <60233436+NithashR@users.noreply.github.com>
2026-01-03 18:35:04 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={process.env.PUBLIC_URL}><App /></BrowserRouter>
+    
   </React.StrictMode>
   
 );
